c7e6de84b5065025dd67e06e50d32996c0e07595
Oliver Meyer <olivermeyer671@gmail.com>
2022-07-23 17:32:42 -0700

    index change 1

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -105,9 +105,9 @@ public class Snake {
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
             response.put("author", ""); // TODO: Your Battlesnake Username
-            response.put("color", "#888888"); // TODO: Personalize
-            response.put("head", "default"); // TODO: Personalize
-            response.put("tail", "default"); // TODO: Personalize
+            response.put("color", "#736CCB"); // TODO: Personalize
+            response.put("head", "beluga"); // TODO: Personalize
+            response.put("tail", "curled"); // TODO: Personalize
             return response;
         }
 
